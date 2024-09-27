5cd7c3dc5799abcc2557f745e690698a67b94c61
Avazkhon <avazkhonnazirov33@gmail.com>
2024-09-28 00:03:12 +0500

    fourth try

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,18 +1,8 @@
-CREATE TEMPORARY TABLE TODELETE as select distinct STUDENT_ID from MARK where MARK >= 4;
-delete from PAYMENT where STUDENT_ID in (select STUDENT_ID from TODELETE);
-delete from MARK where STUDENT_ID in (select STUDENT_ID from TODELETE);
-delete from STUDENT where id in (select STUDENT_ID from TODELETE);
+delete from STUDENT where id in (select STUDENT_ID from MARK where MARK >= 4);
 
-drop table TODELETE;
-
-CREATE TEMPORARY TABLE  TODELETE as select distinct STUDENT_ID from STUDENT join MARK on STUDENT.ID = MARK.STUDENT_ID where MARK > 4 group by STUDENT_ID;
-delete from PAYMENT where STUDENT_ID in (select STUDENT_ID from TODELETE);
-delete from MARK where STUDENT_ID in (select STUDENT_ID from TODELETE);
-delete from STUDENT where id in (select STUDENT_ID from TODELETE);
-
+delete from STUDENT where id in (select MARK.STUDENT_ID from mark group by STUDENT_ID having min(MARK) < 4);
 
 delete from PAYMENT where TYPE_ID = (select ID from PAYMENTTYPE where NAME = 'DAILY');
-delete from PAYMENTTYPE where NAME = 'DAILY';
 
 DELETE FROM MARK WHERE MARK < 7;
 
